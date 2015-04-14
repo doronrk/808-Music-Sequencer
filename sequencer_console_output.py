b832da20f54f1f687086dfce2ac80047b3ce7371
doronrk <doronrk@gmail.com>
2015-04-14 16:47:56 -0400

    added comments to console output file

--- sequencer_console_output.py
+++ sequencer_console_output.py
@@ -1,9 +1,11 @@
 class SequencerConsoleOutput(object):
 
     def __init__(self, sample_files):
+        """Outputs sample names to the console"""
         self.samples = sample_files
 
     def play_samples(self, sample_states):
+        """Outputs the sample names with corresponding index == True in sample_states"""
         for sample, sample_state in zip(self.samples, sample_states):
             if sample_state:
                 print(sample),
